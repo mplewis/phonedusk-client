4be70d805e53e20089168a5d29ba4942beac4cf9
Matthew Lewis <matt@mplewis.com>
2014-11-08 12:55:02 -0600

    Initial Commit

---
+++ Phonedusk/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Phonedusk
+//
+//  Created by Matthew Lewis on 11/8/14.
+//  Copyright (c) 2014 Kestrel Development. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
