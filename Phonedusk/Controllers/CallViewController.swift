1d8b065ecbc0ff880f31cdfd320aef6732893457
Matthew Lewis <matt@mplewis.com>
2014-11-09 01:43:34 -0600

    Start adding ContactsTableVC; improve Call modal behavior

--- Phonedusk/Controllers/CallViewController.swift
+++ Phonedusk/Controllers/CallViewController.swift
@@ -6,36 +6,55 @@
 //  Copyright (c) 2014 Kestrel Development. All rights reserved.
 //
 
+enum CallState {
+    case Dialing, StartedConnecting, Connected, Disconnected, Failed
+}
+
 class CallViewController: UIViewController, CallInfoDelegate {
 
     @IBOutlet weak var dialingText: UILabel!
     @IBOutlet weak var progressSpinner: M13ProgressViewRing!
     
     var numberCalling: String = ""
+    var callState: CallState = .Dialing
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        appDelegate.callInfoDelegate = self
         progressSpinner.indeterminate = true
         progressSpinner.showPercentage = false
-        dialingText.text = "Dialing \(numberCalling)"
+        dialingText.text = "Setting up call..."
     }
 
+    @IBAction func hangUp() {
+        appDelegate.hangUp()
+        if (callState != .StartedConnecting && callState != .Connected) {
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+    
     func callDidStartConnecting(connection: TCConnection!) {
-        
+        dialingText.text = "Calling \(numberCalling)..."
+        callState = .StartedConnecting
     }
     
     func callDidConnect(connection: TCConnection!) {
+        dialingText.text = "Connected to \(numberCalling)"
         progressSpinner.performAction(M13ProgressViewActionSuccess, animated: true)
         progressSpinner.indeterminate = false
+        callState = .Connected
     }
     
     func callDidDisconnect(connection: TCConnection!) {
-        
+        dialingText.text = "Call to \(numberCalling) ended"
+        progressSpinner.performAction(M13ProgressViewActionNone, animated: true)
+        progressSpinner.setProgress(1, animated: false)
+        callState = .Disconnected
     }
     
     func callDidFail(connection: TCConnection!, error: NSError!) {
+        dialingText.text = "Call failed to \(numberCalling)"
         progressSpinner.performAction(M13ProgressViewActionFailure, animated: true)
         progressSpinner.indeterminate = false
+        callState = .Failed
     }
 }
--- Phonedusk/Controllers/ContactsTableViewController.swift
+++ Phonedusk/Controllers/ContactsTableViewController.swift
@@ -6,8 +6,51 @@
 //  Copyright (c) 2014 Kestrel Development. All rights reserved.
 //
 
-import Cocoa
+let sectionTitles = ["Whitelist", "Blacklist"]
 
-class ContactsTableViewController: UITableViewController {
+class ContactsTableViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationItem.rightBarButtonItem = editButtonItem()
+    }
+
+    @IBAction func addContact(sender: AnyObject) {
+        
+    }
+    
+    override func setEditing(editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        if (editing) {
+            let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: Selector("addContact"))
+            navigationItem.leftBarButtonItem = addButton
+        } else {
+            navigationItem.leftBarButtonItem = nil
+        }
+        println("Editing mode: \(editing)")
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ContactCell") as UITableViewCell
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    
 
 }
